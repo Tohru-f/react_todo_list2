8109e53a3ef6f8b9cbac580a0272683187b17c37
Tohru Furusawa <tohru.furusawa@gmail.com>
2025-04-29 20:33:18 +0900

    modify vite.config.js

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_todo_list2/"
+  base: "/react_todo_list2/",
+  build: {
+    outDir: "docs"
+  }
 })
